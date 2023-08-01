06978b48dcb4aee54a71e63620dca2ae362b69b7
KumarSambhav <ksambhav34@gmail.com>
2023-08-01 14:17:49 +0530

    first commit

---
+++ flaskblog/config.py
@@ -0,0 +1,12 @@
+import os
+
+# os.environ.get('SECRET_KEY')
+# os.environ.get('SQLALCHEMY_DATABASE_URI')
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = 'ksambhav21@gmail.com'
+    MAIL_PASSWORD = 'neesjnruxwayibma'

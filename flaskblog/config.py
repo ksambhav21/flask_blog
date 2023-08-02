349f26a75a7af9d050519629c7f190b8d150bb85
KumarSambhav <ksambhav34@gmail.com>
2023-08-02 12:55:46 +0530

    Fourth commit

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -5,6 +5,7 @@ import os
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    # postgres://flask_blog_ftv6_user:jfzHhpJxCIPWg77Kr8hSpOKb6b4hF6Wi@dpg-cj4bkm5gkuvsl082to6g-a.oregon-postgres.render.com/flask_blog_ftv6
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
